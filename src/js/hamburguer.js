22c36fd17b7dff7d3dcec356a6e16ecf03a3a5b3
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-02-09 07:54:53 -0300

    refactor: optimize resize listener in hamburger menu

--- src/js/hamburguer.js
+++ src/js/hamburguer.js
@@ -39,8 +39,6 @@ if (menuHamburguer && navMobile && overlay && header) {
   });
 
   window.addEventListener("resize", () => {
-    ajustarPosicaoMenu();
-
     if (window.innerWidth > 768) {
       fecharMenu();
     }
